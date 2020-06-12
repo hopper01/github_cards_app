d5f3f20f8711e41a5e9dfd9c3d52c7a962b4b4af
hopper01 <anand.dhawan01@gmail.com>
2020-06-12 22:43:34 +0530

    Updates

---
+++ precache-manifest.0c65d78fc42560093d1e03448aceedb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17bc74083b2405f54c6e89756914481",
+    "url": "/github_cards_app/index.html"
+  },
+  {
+    "revision": "ec0ca85a7174f8090e07",
+    "url": "/github_cards_app/static/css/main.e2ac9e9b.chunk.css"
+  },
+  {
+    "revision": "e08492b533236b67a524",
+    "url": "/github_cards_app/static/js/2.7f0359f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github_cards_app/static/js/2.7f0359f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0ca85a7174f8090e07",
+    "url": "/github_cards_app/static/js/main.8e627262.chunk.js"
+  },
+  {
+    "revision": "66a7bab5bf776824e56d",
+    "url": "/github_cards_app/static/js/runtime-main.c8461f82.js"
+  }
+]);
